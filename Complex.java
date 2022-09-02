6add433d10223ec82e7297c99a262c9007384068
anishpawarrr <106865028+anishpawarrr@users.noreply.github.com>
2022-09-02 11:54:55 +0530

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class Complex {
     double a,b;
+    Scanner s = new Scanner(System.in);
+    void input(int i){
 
-    void input(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter real part of complex number : ");
+        System.out.println("Enter real part of complex number "+i+" : ");
         a = s.nextInt();
-        System.out.println("Enter imaginary part of complex number : ");
+        System.out.println("Enter imaginary part of complex number "+i+": ");
         b = s.nextInt();
     }
     void output(){
@@ -33,10 +33,20 @@ public class Complex {
     }
     Complex divide(Complex x, Complex y){
         Complex z = new Complex();
+        double t = y.b;
         y.b = -1*y.b;
         z = z.multiply(x,y);
         z.a = z.a/(y.a*y.a + y.b*y.b);
         z.b = z.b/(y.a*y.a + y.b*y.b);
+        y.b = t;
         return z;
+    }
+    Complex newinp(){
+        Complex c = new Complex();
+        System.out.println("Enter real part of the new number ");
+        c.a = s.nextInt();
+        System.out.println("Enter imaginary part of the new number ");
+        c.b = s.nextInt();
+        return c;
     }
 }
