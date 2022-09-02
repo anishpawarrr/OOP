0fb6873b0d688116443d34de5ab3a16117603cbb
anishpawarrr <106865028+anishpawarrr@users.noreply.github.com>
2022-09-02 11:19:07 +0530

    Create Assignment_1.java

---
+++ Assignment_1.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Assignment_1 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        Complex z0 = new Complex();
+        z0.input();
+        z0.output();
+
+        Complex z1 = new Complex();
+        z1.input();
+        z1.output();
+
+        Complex z2 = new Complex();
+
+        int r = 1;
+        while(r==1){
+            System.out.println("Enter");
+            System.out.println("1 For Addition");
+            System.out.println("2 For Subtraction");
+            System.out.println("3 For Multiplication ");
+            System.out.println("4 For Division");
+            int a = s.nextInt();
+            if(a==1){
+                z2 = z2.add(z1,z0);
+                System.out.print("Addition of ");
+                z2.output();
+            }else if(a==2){
+                z2 = z2.sub(z0,z1);
+                System.out.print("Subtraction of ");
+                z2.output();
+            }else if (a==3){
+                z2 = z2.multiply(z0,z1);
+                System.out.print("Multiplication  of ");
+                z2.output();
+            }else if (a==4) {
+                z2 = z2.divide(z0,z1);
+                System.out.print("Division  of ");
+                z2.output();
+            }else{
+                System.out.println("You entered wrong number :) ");
+            }
+            System.out.println("Enter 1 for further operations or enter any other number to exit");
+            r = s.nextInt();
+        }
+    }
+}
