749e2ddbf804c5f0d50b45936c675a80584fffce
anishpawarrr <106865028+anishpawarrr@users.noreply.github.com>
2022-09-16 12:34:25 +0530

    Update Last.java

--- Last.java
+++ Last.java
@@ -1,3 +1,20 @@
 public class Last extends Ass_proj_manager{
-
+//    String idarr[];
+//    void getsize(int n){
+//        idarr = new String[n];
+//    }
+    boolean checkidd(Last x, Last y){
+        if(x.id.equals(y.id)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    boolean checkmono(Last x, Last y){
+        if(x.mono==y.mono){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
