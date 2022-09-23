c594530ddbf3c13fa6051d1c9c260a5259ac6177
anishpawarrr <106865028+anishpawarrr@users.noreply.github.com>
2022-09-23 12:40:34 +0530

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,15 @@
+abstract class Shape {
+    double area,s1,s2,s3;
+    double compute_area() {
+        return 0;
+    }
+    double gets1(){
+        return s1;
+    }
+    double gets2(){
+        return s2;
+    }
+    double gets3(){
+        return s3;
+    }
+}
